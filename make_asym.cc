a0730818184c1a0a9245eb21b3f6dfecdbe00220
Nate Lust <nlust@astro.princeton.edu>
2016-08-01 15:00:31 -0400

    License code under the MPL v2

--- make_asym.cc
+++ make_asym.cc
@@ -3,6 +3,12 @@
 #include <cmath>
 #include <iostream>
 
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
 namespace py = pybind11;
 using std::pow;
 
