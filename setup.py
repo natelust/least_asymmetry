7586b03a0ce329832402de11fa5acbb142c742dc
Nate Lust <nlust@astro.princeton.edu>
2016-08-01 13:02:18 -0400

    Update setup.py to compile with pybind11
    With the change to pybind11, a new setup.py was needed to handle the
    compiling. The new setup.py is simpler, which is a big gain.

--- setup.py
+++ setup.py
@@ -1,29 +1,25 @@
+import os, sys
+
 from distutils.core import setup, Extension
-from numpy import get_include
-#from numpy.distutils import intelccompiler
-import os
-#import re
+from distutils import sysconfig
 
-#make note, may have to run setup.py clean first!
+cpp_args = ['-std=c++11', '-stdlib=libc++', '-mmacosx-version-min=10.7']
 
-#files = os.listdir('c_code/')
-#this will filter the results for just the c files
-#files = filter(lambda x: not re.search('.+[.]c[~]$',x),files)
+ext_modules = [
+    Extension(
+        'make_asym',
+        ['make_asym.cc'],
+        include_dirs=['include'],
+        language='c++',
+        extra_compile_args=cpp_args,
+    ),
+]
 
-extra = '/opt/local/Library/Frameworks/Python.framework/Versions/Current/lib/'
-extra2 = '/usr/include/'
-ext_mod = []
-inc = [get_include(),extra2,extra]
-
-
-#for i in range(len(files)):
-#    exec('mod'+str(i)+'=Extension("'+files[i].rstrip('.c')+'",sources=["c_code/'+files[i]+'"],include_dirs=inc,extra_compile_args=["-fopenmp"],extra_link_args=["-lgomp"])')
-
-#    exec('ext_mod.append(mod'+str(i)+')')
-
-mod = Extension("make_asym",sources=["make_asym.c"],include_dirs=inc,extra_compile_args=['-undefined dynamic_lookup','-bundle','-L'+extra])#,extra_compile_args=["-fopenmp"],extra_link_args=["-lgomp"])
-#mod = Extension("make_asym",sources=["make_asym.c"],include_dirs=inc,extra_compile_args=[],extra_link_args=[])#,extra_compile_args=["-fopenmp"],extra_link_args=["-lgomp"])
-ext_mod.append(mod)
-
-setup (name='make_asym',version='1.0',description='make asym pyhon c extension',
-       ext_modules = ext_mod)
+setup(
+    name='make_asym',
+    version='0.1',
+    author='Nate Lust',
+    author_email='nlust@astro.princeton.edu',
+    description='A python extension module for calculating asymmetry values',
+    ext_modules=ext_modules,
+)
