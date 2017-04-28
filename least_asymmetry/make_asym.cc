67b39bba5584bb6cf661cb03e2eea515e584c08a
Nate Lust <nlust@astro.princeton.edu>
2017-04-28 16:37:34 -0400

    Change how pybind11 is found

--- least_asymmetry/make_asym.cc
+++ least_asymmetry/make_asym.cc
@@ -1,5 +1,5 @@
-#include <pybind11/pybind11.h>
-#include <pybind11/numpy.h>
+#include "include/pybind11/pybind11.h"
+#include "include/pybind11/numpy.h"
 #include <cmath>
 #include <array>
 #include <vector>
